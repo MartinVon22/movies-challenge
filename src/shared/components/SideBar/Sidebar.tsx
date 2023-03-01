a7331e982caf7b103bc8140c3255ea19636ff79a
Martin <83525340+MartinVon22@users.noreply.github.com>
2023-03-01 04:14:21 -0300

    created componente sidebar with useState for show bg color by pathname

---
+++ src/shared/components/SideBar/Sidebar.tsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+import { faHome, faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useLocation } from 'react-router-dom';
+
+const Sidebar = () => {
+
+    const [routeActive, setRouteActive] = useState('');
+
+    const location = useLocation();
+
+    useEffect(() => {
+        switch (location.pathname) {
+            case '/':
+                setRouteActive('Home');
+                break;        
+            case 'favourites':
+                setRouteActive('Favourites');
+                break;
+            default:
+                break;
+        }
+    }, [])
+
+    return (
+        <div className="flex flex-col h-screen bg-[#1c242c] w-64">
+            <nav className="flex flex-col my-[50%]">
+                <a href="#" className={`text-white transition hover:bg-[#364854] hover:text-white py-2 px-10 rounded ${routeActive === 'Home' ? 'bg-[#364854]' : ''}`}><FontAwesomeIcon icon={faHome} /> Inicio</a>
+                <a href="#" className="text-gray-500 transition hover:bg-[#364854] hover:text-white py-2 rounded px-10"><FontAwesomeIcon icon={faStar} /> Mis favoritos</a>
+            </nav>
+        </div>
+    )
+}
+
+export default Sidebar;
