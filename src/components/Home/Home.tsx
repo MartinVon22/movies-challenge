7d92ded27b89b2adfefcd04a41fea4747af1dbe7
Martin <83525340+MartinVon22@users.noreply.github.com>
2023-03-01 02:32:37 -0300

    created simple sidebar on HomeView

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,14 @@
 const HomeView = () => {
 
     return (
-        <p>Home view</p>
+        <div className="flex flex-col h-screen bg-gray-800 w-64">
+            <nav className="flex flex-col mt-10">
+                <a href="#" className="text-gray-400 hover:text-white py-2 px-4">Dashboard</a>
+                <a href="#" className="text-gray-400 hover:text-white py-2 px-4">Usuarios</a>
+                <a href="#" className="text-gray-400 hover:text-white py-2 px-4">Productos</a>
+                <a href="#" className="text-gray-400 hover:text-white py-2 px-4">Configuración</a>
+            </nav>
+        </div>
     )
 
 }
