0bf711d8038e71351def2b05e67fca76c7d6b1a1
Martin <83525340+MartinVon22@users.noreply.github.com>
2023-03-01 04:14:57 -0300

    modifications on Home view and imported Sidebar component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,14 +1,26 @@
+import { faHome, faStar, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Sidebar from "../../shared/components/SideBar/Sidebar";
+
 const HomeView = () => {
 
     return (
-        <div className="flex flex-col h-screen bg-gray-800 w-64">
-            <nav className="flex flex-col mt-10">
-                <a href="#" className="text-gray-400 hover:text-white py-2 px-4">Dashboard</a>
-                <a href="#" className="text-gray-400 hover:text-white py-2 px-4">Usuarios</a>
-                <a href="#" className="text-gray-400 hover:text-white py-2 px-4">Productos</a>
-                <a href="#" className="text-gray-400 hover:text-white py-2 px-4">Configuración</a>
-            </nav>
-        </div>
+        <>
+            <div className="flex">
+                <Sidebar />
+                <div className="flex w-[100%] h-screen bg-[#1c242c] justify-between">
+                    <div className="text-2xl mt-10 mx-40">
+                        <p className="text-white">Películas populares</p>
+                    </div>
+                    <div className="relative mt-10 mr-20">
+                        <span className="absolute left-3 top-2">
+                            <FontAwesomeIcon icon={faMagnifyingGlass} color='#ccc'/>
+                        </span>
+                        <input type="text" className="bg-[#1c242c] border border-slate-700 rounded px-10 py-2 text-gray-300 focus:outline-0" placeholder="Buscar película.." />
+                    </div>
+                </div>
+            </div>
+        </>
     )
 
 }
