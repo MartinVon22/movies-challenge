0a72e2ab2b9b9cf8511997859b021d888e9d03cc
Martin <83525340+MartinVon22@users.noreply.github.com>
2023-03-02 02:27:57 -0300

    modified design of sidebar and taked menu status from context

--- src/shared/components/SideBar/Sidebar.tsx
+++ src/shared/components/SideBar/Sidebar.tsx
@@ -7,8 +7,7 @@ import { AppContext } from '../../../context/AppContext';
 const Sidebar = () => {
 
     const [routeActive, setRouteActive] = useState('');
-    const [menuIsOpen, setMenuIsOpen] = useState(false);
-    const { dispatch } = useContext(AppContext);
+    const { state, dispatch } = useContext(AppContext);
 
     const location = useLocation();
 
@@ -25,16 +24,16 @@ const Sidebar = () => {
         }
     }, [location])
 
+
     const handleShowMenu = () => {
-        setMenuIsOpen(!menuIsOpen)
-        dispatch({ type: !menuIsOpen ? "OPEN_MENU" : "CLOSE_MENU" });
+        dispatch({ type: !state.menuIsOpen ? "OPEN_MENU" : "CLOSE_MENU" });
 
     }
 
     return (
         <>
-            <span className={`absolute z-40 top-5 transition-all ${!menuIsOpen ? 'left-10' : 'left-56'} hover:cursor-pointer`} onClick={handleShowMenu}><FontAwesomeIcon  icon={menuIsOpen ? faClose: faBars} color='white' size={'lg'} /></span>
-            <div className={`flex flex-col h-screen absolute bg-[#1c242c] w-64 z-10 transition-all ${!menuIsOpen ? '-ml-[100%]' : 'ml-[0%]'}`}>
+            <span className={`absolute z-40 top-5 transition-all ${!state.menuIsOpen ? 'left-10' : 'left-56'} hover:cursor-pointer`} onClick={handleShowMenu}><FontAwesomeIcon  icon={state.menuIsOpen ? faClose: faBars} color='white' size={'lg'} /></span>
+            <div className={`flex flex-col fixed h-[100%] bg-[#1c242c] w-64 z-10 transition-all ${!state.menuIsOpen ? '-ml-[100%]' : 'ml-[0%]'}`}>
                 <nav className="flex flex-col my-[50%]">
                     <span className={`transition hover:bg-[#364854] hover:text-white hover:cursor-pointer py-2 px-10 rounded ${routeActive === 'Home' ? 'bg-[#364854] text-white' : 'text-gray-500'}`}><FontAwesomeIcon icon={faHome} /> Inicio</span>
                     <span className={`transition hover:bg-[#364854] hover:text-white hover:cursor-pointer py-2 px-10 rounded ${routeActive === 'Favourites' ? 'bg-[#364854] text-white' : 'text-gray-500'}`}><FontAwesomeIcon icon={faStar} /> Mis favoritos</span>
