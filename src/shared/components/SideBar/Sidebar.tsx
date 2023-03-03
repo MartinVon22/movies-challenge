e15172fb26eb9fbee91c2560fad8450c22895335
Martin <83525340+MartinVon22@users.noreply.github.com>
2023-03-03 01:00:15 -0300

    customized sidebar

--- src/shared/components/SideBar/Sidebar.tsx
+++ src/shared/components/SideBar/Sidebar.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useContext } from 'react';
 import { faBars, faClose, faHome, faStar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { AppContext } from '../../../context/AppContext';
 
 const Sidebar = () => {
@@ -10,6 +10,7 @@ const Sidebar = () => {
     const { state, dispatch } = useContext(AppContext);
 
     const location = useLocation();
+    const navigate = useNavigate();
 
     useEffect(() => {
         switch (location.pathname) {
@@ -32,11 +33,11 @@ const Sidebar = () => {
 
     return (
         <>
-            <span className={`absolute z-40 top-5 transition-all ${!state.menuIsOpen ? 'left-10' : 'left-56'} hover:cursor-pointer`} onClick={handleShowMenu}><FontAwesomeIcon  icon={state.menuIsOpen ? faClose: faBars} color='white' size={'lg'} /></span>
+            <span className={`fixed z-40 top-5 transition-all ${!state.menuIsOpen ? 'left-8' : 'left-56'} hover:cursor-pointer`} onClick={handleShowMenu}><FontAwesomeIcon  icon={state.menuIsOpen ? faClose: faBars} color='white' size={'lg'} /></span>
             <div className={`flex flex-col fixed h-[100%] bg-[#1c242c] w-64 z-10 transition-all ${!state.menuIsOpen ? '-ml-[100%]' : 'ml-[0%]'}`}>
                 <nav className="flex flex-col my-[50%]">
-                    <span className={`transition hover:bg-[#364854] hover:text-white hover:cursor-pointer py-2 px-10 rounded ${routeActive === 'Home' ? 'bg-[#364854] text-white' : 'text-gray-500'}`}><FontAwesomeIcon icon={faHome} /> Inicio</span>
-                    <span className={`transition hover:bg-[#364854] hover:text-white hover:cursor-pointer py-2 px-10 rounded ${routeActive === 'Favourites' ? 'bg-[#364854] text-white' : 'text-gray-500'}`}><FontAwesomeIcon icon={faStar} /> Mis favoritos</span>
+                    <span onClick={() => navigate('/')} className={`transition hover:bg-[#364854] hover:text-white hover:cursor-pointer py-2 px-10 rounded ${routeActive === 'Home' ? 'bg-[#364854] text-white' : 'text-gray-500'}`}><FontAwesomeIcon icon={faHome} /> Inicio</span>
+                    <span onClick={() => navigate('/')} className={`transition hover:bg-[#364854] hover:text-white hover:cursor-pointer py-2 px-10 rounded ${routeActive === 'Favourites' ? 'bg-[#364854] text-white' : 'text-gray-500'}`}><FontAwesomeIcon icon={faStar} /> Mis favoritos</span>
                 </nav>
             </div>
         </>
