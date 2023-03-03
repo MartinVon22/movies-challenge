c8801dd92dbc470d08cd385ad28023cdc8668664
Martin <83525340+MartinVon22@users.noreply.github.com>
2023-03-03 00:59:16 -0300

    added services for getMovies, createGuestSession and rateMovie

--- src/core/services/movies.service.ts
+++ src/core/services/movies.service.ts
@@ -3,3 +3,16 @@ import axios from "axios";
 export const getPopularMovies = (page: number) => {
     return axios.get(`${process.env.REACT_APP_API_ENDPOINT}/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${page}`);
 }
+
+export const getMovieSearch = (movie: string, page: number) => {
+    return axios.get(`${process.env.REACT_APP_API_ENDPOINT}/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${page}&query=${movie}`);
+}
+
+
+export const createGuestSession = () => {
+    return axios.get(`${process.env.REACT_APP_API_ENDPOINT}/authentication/guest_session/new?api_key=${process.env.REACT_APP_API_KEY}`);
+}
+
+export const rateMovie = (rateValue: number, guest_session_id: string, movieId: number) => {
+    return axios.post(`${process.env.REACT_APP_API_ENDPOINT}/movie/${movieId}/rating?api_key=${process.env.REACT_APP_API_KEY}?guest_session_id=${guest_session_id}`, {value: rateValue});
+}
